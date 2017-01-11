4c20ba77afabf91d27dd95b317873aceb0d110eb
Clément Delalande <delalande.clem@gmail.com>
2017-01-11 16:33:43 +0100

    Updated to 0.3 - Change behavior like on official server * Remove cli commands * Done some fixes and optimisations

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 /*
 * Author: Clement Delalande (https://github.com/iKlem)
-* Version: 0.2.1
+* More informations on the github repo (https://github.com/ScreepsMods/screepsmod-gcltocpu)
+* Version: 0.3.0
 */
 
 module.exports = (config) => {
   const constants = config.common.constants,
-  storage = config.common.storage,
-  cli = config.cli,
-  engine = config.engine;
+    storage = config.common.storage,
+    engine = config.engine;
 
   let gclMult = 1,
-  gclPow = 1,
-  gclLevel = 1,
-  newCPU = 0,
-  oldUsers,
-  doUpdate,
-  updateUser;
+    gclLevel = 1,
+    gclPow = 1,
+    newCPU = 0,
+    oldUsers;
 
-  doUpdate = (forceUpdate = false) => {
+  /**
+  * Update the CPU with the change of GCL.
+  *
+  * @param {boolean} forceUpdate - Force the update instead of waiting a change.
+  */
+  function doUpdate() {
     if (engine) {
       oldUsers = engine.driver.getAllUsers;
 
@@ -29,12 +32,7 @@ module.exports = (config) => {
             if(!user.bot) {
               gclLevel = Math.floor(Math.pow((user.gcl || 0) / gclMult, 1 / gclPow)) + 1,
               newCPU = gclLevel * 10 + 20
-              // TODO: Add check for errors
-              if (!forceUpdate) {
-                if (newCPU > user.cpu) {
-                  updateUser(user, newCPU);
-                }
-              } else {
+              if ((user.cpu === 100 && newCPU < user.cpu) || newCPU > user.cpu) {
                 updateUser(user, newCPU);
               }
             }
@@ -45,7 +43,13 @@ module.exports = (config) => {
     }
   };
 
-  updateUser = (user, cpu) => {
+  /**
+  * Update the new cpu from the storage db
+  *
+  * @param {Object} user - The user to update
+  * @param {number} cpu - The new value for the CPU
+  */
+  function updateUser(user, cpu) {
     if(storage) {
       console.log(`Updated maximum cpu to ${cpu} for user ${user.username}`);
       let retVal = storage.db.users.update({username: user.username}, {$set: {cpu: cpu}});
@@ -56,34 +60,12 @@ module.exports = (config) => {
     return false;
   };
 
+  // main (looping by the engine runners)
   if (constants) {
     gclMult = constants.GCL_MULTIPLY;
     gclPow = constants.GCL_POW;
 
     doUpdate();
-
-    if (cli) {
-      cli.on("cliSandbox", (sandbox) => {
-        sandbox.updateCPU = (playerName = "") => {
-          if (storage) {
-            if (playerName != "") { // If a player name is given, update only for the player
-              return storage.db.users.findOne({username: playerName}).then((user) => {
-                if(user) {
-                  gclLevel = Math.floor(Math.pow((user.gcl || 0) / gclMult, 1 / gclPow)) + 1,
-                  newCPU = gclLevel * 10 + 20
-                  sandbox.print(`Updating cpu for ${user.username} to ${newCPU}...`);
-                  return updateUser(user, newCPU) ? "Done" : `Error updating cpu for ${user.username}!`;
-                }
-              });
-            } else { // If no player name, update all players CPU
-              sandbox.print(`Updating cpu for all users to their GCL...`);
-              doUpdate(true);
-              return "Update finished!";
-            }
-          }
-        }
-      });
-    }
   }
 
 };
